482009e650e89faf39c0c54553468f1e39f1cdbb
Paul Schreiber <paul@techmatters.org>
2024-04-08 17:29:48 -0400

    fix: move HWSD to global section of config

--- soil_id/config.py
+++ soil_id/config.py
@@ -13,8 +13,7 @@ OUTPUT_PATH = TEMP_DIR.name
 SOIL_ID_RANK_PATH = f"{OUTPUT_PATH}/soil_id_rank.csv"
 SOIL_ID_PROB_PATH = f"{OUTPUT_PATH}/soil_id_cond_prob.csv"
 
-# Shared data
-HWSD_PATH = f"{DATA_PATH}/HWSD_global_noWater_no_country.shp"
+# Determines if in/out of US
 US_AREA_PATH = f"{DATA_PATH}/SoilID_US_Areas.shp"
 
 # US Soil ID
@@ -22,6 +21,7 @@ STATSGO_PATH = f"{DATA_PATH}/gsmsoilmu_a_us.shp"
 MUNSELL_RGB_LAB_PATH = f"{DATA_PATH}/LandPKS_munsell_rgb_lab.csv"
 
 # Global Soil ID
+HWSD_PATH = f"{DATA_PATH}/HWSD_global_noWater_no_country.shp"
 WISE_PATH = f"{DATA_PATH}/wise30sec_poly_simp_soil.shp"
 NORM_DIST_1_PATH = f"{DATA_PATH}/NormDist1.csv"
 NORM_DIST_2_PATH = f"{DATA_PATH}/NormDist2.csv"
